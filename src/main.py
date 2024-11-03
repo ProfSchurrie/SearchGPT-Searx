0f004078825698870bf601106f7a1b86b7bdfe22
ProfSchurrie <sorencoremans@yahoo.de>
2024-11-03 08:24:37 +0100

    test

--- src/main.py
+++ src/main.py
@@ -25,7 +25,6 @@ if __name__ == "__main__":
     ai_message_obj = content_processor.get_answer(query, formatted_relevant_docs, searx_response['language'], output_format, profile)
     answer = ai_message_obj.content + '\n'
     end = time.time()
-    print("\n\nGPT Answer time:", end - start, "s")
 
     # Optional Part: display the reference sources of the quoted sentences in LLM's answer
     # 
